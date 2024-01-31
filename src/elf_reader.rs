d10883a7483139910e2ccbe4e9204149ec7041f6
Ming Xu <xumingxs@qq.com>
2024-01-31 19:12:07 +0800

    杂七杂八的东西

--- src/elf_reader.rs
+++ src/elf_reader.rs
@@ -48,12 +48,12 @@ impl ElfReader {
         let io = File::open(file).expect("Can not open file");
         let mut file_stream = ElfStream::<AnyEndian, _>::open_stream(io).expect("Open Failed");
 
-        let text_shdr = *file_stream.section_header_by_name(".text")
-        .expect("Section table should be parseable")
-        .expect("File should have a .text section");
+        // let text_shdr = *file_stream.section_header_by_name(".text")
+        // .expect("Section table should be parseable")
+        // .expect("File should have a .text section");
         // 不确定.text文件中是否就是代码，它的起始和结束是否就是在代码的起始的结束
-        let start = text_shdr.sh_addr;
-        let end = start + text_shdr.sh_offset;
+        // let start = text_shdr.sh_addr;
+        // let end = start + text_shdr.sh_offset;
 
         let (sym_t, str_t) = file_stream.symbol_table()
         .expect("Section table should be parseable")
@@ -67,9 +67,9 @@ impl ElfReader {
 
             let func_type = if func_start == func_end && func_start == 0 { FunType::ExternalFunc } 
             else { FunType::LocalFunc };
-
+            // 似乎end是开区间
             Func { id: idx as u32, func_type, name: func_name.to_string(), start: func_start, end: func_end }
-        }).collect::<Vec<Func>>();
+        }).filter(|x| x.func_type == FunType::LocalFunc).collect::<Vec<Func>>();
 
         func_vec.sort_by(|a, b| {
             if a.func_type == FunType::ExternalFunc {
@@ -89,8 +89,11 @@ impl ElfReader {
             if x.start == x.end && x.func_type == FunType::LocalFunc {
                 debug_print!("----- ");
             }
-            debug_println!("Get function: {}, id: {}, start: {}, end: {}, type: {:?}", x.name, x.id, x.start, x.end, x.func_type);
+            debug_println!("Get function: {}, id: {}, start: 0x{:X}, end: 0x{:X}, type: {:?}", x.name, x.id, x.start, x.end, x.func_type);
         });
+
+        let start = func_vec.first().expect("Failed to get the first elements in func_vec").start;
+        let end = func_vec.last().expect("Failed to get the last elements in func_vec").end;
 
         ElfReader {
             id,
@@ -102,20 +105,68 @@ impl ElfReader {
             call_tracer: Vec::new(),
         }
     }
+
+    fn find(&self, value: u64) -> Option<&Func> {
+        self.func_vec.binary_search_by(|x| {
+            if value < x.start {
+                Ordering::Greater
+            } else if value >= x.end {
+                Ordering::Less
+            } else {
+                Ordering::Equal
+            }
+        }).ok().and_then(|idx| self.func_vec.get(idx))
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::Rng;
+
+    fn create_new(id: u32, path: &str) -> ElfReader {
+        let file = PathBuf::from(path);
+        ElfReader::new(id, &file)
+    }
 
     #[test]
+    // 这是x86的elf
     fn test_reader_new() {
-        let file = PathBuf::from("./test_elf/riscv64-nemu-interpreter");
-        let elf_reader0 = ElfReader::new(0, &file);
-        println!("ElfReader(id: {}, file: {}, start: {}, end: {}, func_num: {})", 
+        let elf_reader0 = create_new(0, "./test_elf/riscv64-nemu-interpreter");
+        println!("ElfReader(id: {}, name: {}, start: 0x{:X}, end: 0x{:X}, func_num: {})", 
         elf_reader0.id, elf_reader0.name, elf_reader0.start, elf_reader0.end, elf_reader0.func_vec.len());
         for i in 0..elf_reader0.func_vec.len() {
             assert!(elf_reader0.func_vec[i].id == i as u32);
+        }
+    }
+
+    #[test]
+    // 这是riscv的elf
+    fn test_reader_new1() {
+        let elf_reader1 = create_new(1, "./test_elf/nanos-lite-riscv64-nemu.elf");
+        println!("ElfReader(id: {}, name: {}, start: 0x{:X}, end: 0x{:X}, func_num: {})", 
+        elf_reader1.id, elf_reader1.name, elf_reader1.start, elf_reader1.end, elf_reader1.func_vec.len());
+        for i in 0..elf_reader1.func_vec.len() {
+            assert!(elf_reader1.func_vec[i].id == i as u32);
+        }
+    }
+
+    #[test]
+    fn test_find1() {
+        let elf_reader1 = create_new(1, "./test_elf/nanos-lite-riscv64-nemu.elf");
+        let mut rng = rand::thread_rng();
+        println!("ElfReader(id: {}, name: {}, start: 0x{:X}, end: 0x{:X}, func_num: {})", 
+        elf_reader1.id, elf_reader1.name, elf_reader1.start, elf_reader1.end, elf_reader1.func_vec.len());
+
+        println!("\n===============To Test hit================");
+        for i in 0..elf_reader1.func_vec.len() {
+            let start = elf_reader1.func_vec[i].start;
+            let end = elf_reader1.func_vec[i].end;
+            if start == end { continue; }
+            let rand_addr = rng.gen_range(start..end);
+            print!("Random addr: 0x{:X}, start: 0x{:X}, end: 0x{:X}\t", rand_addr, start, end);
+            assert!(elf_reader1.find(rand_addr).is_some());
+            println!("Hit!");
         }
     }
 }
