b1079353075ed18927f084e8bbca52cad3b903e6
徐铭 <xumingxs@qq.com>
2024-02-08 21:41:44 +0800

    添加构造函数和cur_reader读取函数

--- src/manager.rs
+++ src/manager.rs
@@ -1,5 +1,9 @@
 use crate::elf_reader::*;
 
+enum CurReader {
+    MainReader,
+    ProgsReader(usize),
+}
 
 struct FuncInstance {
     id: u32,
@@ -24,20 +28,53 @@ impl FuncInstance {
 }
 
 
-struct manager<'a> {
+struct manager {
     show_context: bool,
     main_reader: ElfReader,
-    cur_reader: &'a ElfReader,
+    cur_reader: CurReader,
     prog_readers: Option<Vec<ElfReader>>,
     trace_log: Vec<FuncInstance>,
     func_stack: Vec<FuncInstance>,
 }
 
-// impl manager {
-//     fn new(show_context: bool, main_path: &str, progs_path: Option<Vec<&str>>) {
-//         let main_reader = ElfReader::new(0, );
-
-//         let progs_reader = Vec::new();
+impl manager {
+    fn new(show_context: bool, main_path: &str, progs_path: Option<Vec<&str>>) -> Self {
+        let main_reader = ElfReader::new(0, main_path);
+        let prog_readers = if let Some(x) = progs_path {
+            let mut prog_readers: Vec<ElfReader> = Vec::new();
+            for (idx, i) in x.into_iter().enumerate() {
+                prog_readers.push(ElfReader::new(idx as u32, i));
+            }
+            Some(prog_readers)
+        } else {
+            None
+        };
         
-//     }
-// }
+        manager {
+            show_context,
+            main_reader,
+            cur_reader: CurReader::MainReader,
+            prog_readers,
+            trace_log: Vec::new(),
+            func_stack: Vec::new(),
+        }
+    }
+
+    fn cur_reader(&self) -> &ElfReader {
+        match self.cur_reader {
+            CurReader::MainReader => &self.main_reader,
+            CurReader::ProgsReader(x) => {
+                let res = &self.prog_readers
+                .as_ref()
+                .expect("Option<Vec> is None, should not reach ProgsReader arm")[x]; 
+                if res.id as usize == x {
+                    res
+                } else {
+                    panic!("Reader id is not compatable with its index in the vec")
+                }
+            }
+        }
+    }
+
+    
+}
